da278bd0fdd22c7c9b385309312897c560822169
Muhammad Shoaib <muhammad.shoaib@psw.gov.pk>
2025-12-17 10:27:47 +0500

    added global url

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,41 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    federation({
-      name: 'custom-main',
-      remotes: {
-        userManagement: 'http://localhost:5001/assets/remoteEntry.js',
-        tariff: 'http://localhost:5002/assets/remoteEntry.js',
-      },
-      shared: ['react', 'react-dom']
-    })
-  ],
-  build: {
-    modulePreload: false,
-    target: 'esnext',
-    minify: false,
-    cssCodeSplit: false
-  },
-  server: {
-    port: 5000,
-    strictPort: true,
-  },
-  preview: {
-    port: 5000,
-    strictPort: true,
+export default defineConfig(({ mode }) => {
+  // Load env variables based on mode (development / production)
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      federation({
+        name: 'custom-main',
+        remotes: {
+          userManagement: env.VITE_USER_REMOTE,
+          tariff: env.VITE_TARIFF_REMOTE,
+        },
+        shared: ['react', 'react-dom'],
+      }),
+    ],
+
+    build: {
+      modulePreload: false,
+      target: 'esnext',
+      minify: false,
+      cssCodeSplit: false,
+    },
+
+    server: {
+      port: 5000,
+      strictPort: true,
+    },
+
+    preview: {
+      port: 5000,
+      strictPort: true,
+    },
   }
 })
